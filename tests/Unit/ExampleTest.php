c5644d0c0b8a2cd3c00df1b3edca32239ae8a115
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-11-15 08:35:30 +0200

    initial commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertTrue;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        assertTrue(true);
+    }
+}
